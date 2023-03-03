366b9a44b678bce9ef6880b31406ed91b23cc9b6
Rheiley <rheileyzoom@gmail.com>
2023-03-02 18:30:52 -0800

    Create AppTracker.java

---
+++ AppTracker.java
@@ -0,0 +1,23 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.io.File;
+import java.nio.file.WatchService;
+
+public class AppTracker{
+    private static int count = 0;
+    private File file;
+    public static void main(String[] args) throws IOException{
+        
+        
+    }
+
+
+
+    public static void trackFile(File file) throws IOException{
+        FileInputStream in = new FileInputStream(File);
+
+    }
+
+
+}
